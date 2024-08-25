0440a676e939643d868b7b082f349231cedf0796
Abubakr <abubakrdev69@gmail.com>
2024-08-25 19:26:33 +0500

    add all pages

--- src/components/lazy/LazyLoading.jsx
+++ src/components/lazy/LazyLoading.jsx
@@ -2,12 +2,10 @@ import React, { memo } from "react";
 import { FaSpinner } from "react-icons/fa";
 import "./LazyLoading.scss";
 
-const LazyLoading = () => {
-  return (
-    <div className="lazy-loading">
-      <FaSpinner className="lazy-loading__spinner" />
-    </div>
-  );
-};
+const LazyLoading = () => (
+  <div className="lazy-loading">
+    <FaSpinner className="lazy-loading__spinner" />
+  </div>
+);
 
 export default memo(LazyLoading);
